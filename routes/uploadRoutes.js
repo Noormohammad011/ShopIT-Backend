669313b141a38fbda22326299f38ffc9e2c03c50
Noor Mohammad <noormohammad.net1503011@gmail.com>
2023-01-22 16:43:40 +0600

    image-url

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,41 +1,39 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
 import path from 'path'
-import express from 'express'
-import multer from 'multer'
-const router = express.Router()
-
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, 'uploads/')
-  },
-  filename(req, file, cb) {
-    cb(
-      null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    )
-  },
-})
-
-function checkFileType(file, cb) {
-  const filetypes = /jpg|jpeg|png/
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
-  const mimetype = filetypes.test(file.mimetype)
-
-  if (extname && mimetype) {
-    return cb(null, true)
-  } else {
-    cb('Images only!')
-  }
-}
-
-const upload = multer({
-  storage,
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb)
-  },
-})
-
-router.post('/', upload.single('image'), (req, res) => {
-  res.send(`/${req.file.path}`)
-})
-
-export default router
+const uploadImage =
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (!product) {
+      res.status(404)
+      throw new Error('Product not found')
+    }
+    if (!req.files) {
+      res.status(400)
+      throw new Error('Please upload a file')
+    }
+    const file = req.files.file
+    if (!file.mimetype.startsWith('image')) {
+      res.status(400)
+      throw new Error('Please upload an image file')
+    }
+    if (file.size > process.env.MAX_FILE_UPLOAD) {
+      res.status(400)
+      throw new Error(
+        `Please upload an image less than ${process.env.MAX_FILE_UPLOAD}`
+      )
+    }
+    file.name = `photo_${product._id}${path.parse(file.name).ext}`
+    file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async (err) => {
+      if (err) {
+        console.error(err)
+        res.status(500)
+        throw new Error('Problem with file upload')
+      }
+      const imageUrl = `images/${file.name}`
+      await Product.findByIdAndUpdate(req.params.id, { image: imageUrl })
+      res.status(200).json(file.name)
+    })
+  }) 
+  
+export default uploadImage
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import cors from 'cors'
+import fileUpload from 'express-fileupload'
 import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
@@ -10,7 +11,7 @@ import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
-import uploadRoutes from './routes/uploadRoutes.js'
+import uploadImage from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -26,11 +27,14 @@ app.use(express.json())
 // Enable cors
 app.use(cors())
 
+//file upload
+app.use(fileUpload())
+
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
-app.use('/api/upload', uploadRoutes)
+app.post('/api/upload/:id', uploadImage)
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
