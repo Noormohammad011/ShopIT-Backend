b4bde7c7254fa83adc20fe8022e41e7af8505e0f
Noor Mohammad <noormohammad.net1503011@gmail.com>
2023-01-23 01:31:16 +0600

    creating tmp folder

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get('/api/config/paypal', (req, res) =>
 )
 
 const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+app.use('/upload', express.static(path.join(__dirname, '/tmp')))
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
